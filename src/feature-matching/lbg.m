0a643fa9ccac64146f96b834386e4c8b50ded40d
Franco Prudhomme <francoprud@gmail.com>
2016-11-18 18:00:58 -0300

    Fixes small bug in LBG implementation

--- src/feature-matching/lbg.m
+++ src/feature-matching/lbg.m
@@ -1,4 +1,4 @@
-% lbg 
+% lbg
 % computes a codeBook using the LBG vector quantization algorithm
 
 % training: mel frecuency cepstral coeficients
@@ -10,33 +10,33 @@ function codebook = lbg(training, epsilon,nCodeVectors)
 	N = 1;
 	M = length(training(:,1));
 	k = length(training(1,:));
-	
+
 	% set codevector with average of etire training sequence
 	codebook(1,:) =  mean(training);
-	
+
 	% Dstar = codebook average distortion
 	Dstar  =  0;
-	for m = 1 : M	
+	for m = 1 : M
 		Dstar += norm(training(m,:) - codebook(1,:)).^2;
 	end
-	Dstar *= (1/M*k);
+	Dstar *= (1/(M*k));
 
 	% loop until the amount of codevectors is reached
 	i=0;
 	while  N < nCodeVectors
-	
+
 		% SPLITTING
 		% double amount of codevectors
 		for i = 1:N
 			c(i,:) = (1+epsilon)*codebook(i,:);
 			c(N+i,:) =  (1-epsilon)*codebook(i,:);
 		end
-	
+
 		N = 2 * N;
-		
+
 		% ITERATION
-		% initial distortion	
-		d = Dstar; 
+		% initial distortion
+		d = Dstar;
 		% iterate until the codebook converges
 		do
 			% D: previous step distortion
@@ -44,7 +44,7 @@ function codebook = lbg(training, epsilon,nCodeVectors)
 			% Q: mapping from training data to codevector
 			% indexes
 			Q = getMapping(c,training);
-		
+
 			for n = 1:N
 				% q: indexes of training samples that
 				% map to c(n)
@@ -56,11 +56,10 @@ function codebook = lbg(training, epsilon,nCodeVectors)
 			d = getDistortion(c,training,Q);
 
 		until (D - d)/D < epsilon
-		
+
 		% set new codebook distortion
 		Dstar = d;
 		% set new codebook
 		codebook = c;
 	end
 end
-
